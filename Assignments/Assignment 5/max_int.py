b3281d08edb179bff62ee51d453b559606901287
Jón Ágúst Guðmundsson <jonag15@ru.is>
2019-09-05 14:02:49 +0000

    Fixed missing : from while and if loop in max_int

--- Assignments/Assignment 5/max_int.py
+++ Assignments/Assignment 5/max_int.py
@@ -4,8 +4,8 @@ num_int = int(input("Input a number: "))    # Do not change this line
 
 max_int = num_int
 #compare new input to original and ask for new input
-while num_int >= 0
-    if num_int > max_int
+while num_int >= 0:
+    if num_int > max_int:
         max_int = num_int
     num_int = int(input("Input a number: "))
 
