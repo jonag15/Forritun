1cb7ec6b8ef72a9368367cc1934d365edfbd4c4b
Jón Ágúst Guðmundsson <jonag15@ru.is>
2019-09-05 13:58:08 +0000

    Add while loop to compare values in max_int

--- Assignments/Assignment 5/max_int.py
+++ Assignments/Assignment 5/max_int.py
@@ -2,4 +2,8 @@
 
 num_int = int(input("Input a number: "))    # Do not change this line
 
+max_int = num_int
+#compare new input to original
+while num_int >= 0
+
 print("The maximum is", max_int)    # Do not change this line
