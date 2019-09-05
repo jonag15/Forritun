01f315e459d33b88caaeca601b1973329dd7fcbf
Jón Ágúst Guðmundsson <jonag15@ru.is>
2019-09-05 13:51:22 +0000

    Assignment defined for max_int

---
+++ Assignments/Assignment 5/max_int.py
@@ -0,0 +1,5 @@
+#find the maximum positive integer input until a negative value is entered
+
+num_int = int(input("Input a number: "))    # Do not change this line
+
+print("The maximum is", max_int)    # Do not change this line
