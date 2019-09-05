e2a2d2ac22b11ae1ece9e0767a0a461a224587d0
Jón Ágúst Guðmundsson <jonag15@ru.is>
2019-09-05 14:00:38 +0000

    First try of while loop solution for max_int

--- Assignments/Assignment 5/max_int.py
+++ Assignments/Assignment 5/max_int.py
@@ -3,7 +3,10 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 
 max_int = num_int
-#compare new input to original
+#compare new input to original and ask for new input
 while num_int >= 0
+    if num_int > max_int
+        max_int = num_int
+    num_int = int(input("Input a number: "))
 
 print("The maximum is", max_int)    # Do not change this line
