7a5807bc1087984e13ffa8f15e813e0b8a389320
Jón Ágúst Guðmundsson <jonag15@ru.is>
2019-09-05 14:27:05 +0000

    Assignment defined

---
+++ Assignments/Assignment 5/sequence.py
@@ -0,0 +1,4 @@
+#Takes input n and generates the first n numbers of a formula that starts at one and then adds the three latest numbers
+#together to create a new number, starts by adding the available numbers e.g. 1, 1+1=2, 1+2=3
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
