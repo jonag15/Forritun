dcacec6b80900c936738faa42864e440c13284e9
Jón Ágúst Guðmundsson <jonag15@ru.is>
2019-09-05 14:32:59 +0000

    First configueration of solution

--- Assignments/Assignment 5/sequence.py
+++ Assignments/Assignment 5/sequence.py
@@ -2,3 +2,9 @@
 #together to create a new number, starts by adding the available numbers e.g. 1, 1+1=2, 1+2=3
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+#define the starting numbers
+first_number = 0
+second_number = 0
+third_number = 1
+
+for i in range(0,n)
